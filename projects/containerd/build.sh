659102fed78d0b87dba3d4efc038b7dde34b367e
0roman <roman.wagner18@gmx.de>
2022-01-06 22:41:35 +0100

    containerd add instrumentation filter

--- projects/containerd/build.sh
+++ projects/containerd/build.sh
@@ -15,5 +15,97 @@
 #
 ################################################################################
 
-$SRC/containerd/contrib/fuzz/oss_fuzz_build.sh
+#$SRC/containerd/contrib/fuzz/oss_fuzz_build.sh
+cd $SRC/containerd/contrib/fuzz/
+set -o nounset
+set -o pipefail
+set -o errexit
+set -x
 
+#cd "$(dirname "${BASH_SOURCE[0]}")"
+cd ../../
+
+# Move all fuzzers that don't have the "fuzz" package out of this dir
+mv contrib/fuzz/docker_fuzzer.go remotes/docker/
+mv contrib/fuzz/container_fuzzer.go integration/client/
+
+
+# Change path of socket since OSS-fuzz does not grant access to /run
+sed -i 's/\/run\/containerd/\/tmp\/containerd/g' $SRC/containerd/defaults/defaults_unix.go
+
+# To build FuzzContainer2 we need to prepare a few things:
+# We change the name of the cmd/containerd package
+# so that we can import it.
+# We furthermore add an exported function that is similar
+# to cmd/containerd.main and call that instead of calling
+# the containerd binary.
+#
+# In the fuzzer we import cmd/containerd as a low-maintenance
+# way of initializing all the plugins.
+# Make backup of cmd/containerd:
+cp -r $SRC/containerd/cmd/containerd $SRC/cmd-containerd-backup
+# Rename package:
+find $SRC/containerd/cmd/containerd -type f -exec sed -i 's/package main/package mainfuzz/g' {} \;
+# Add an exported function
+sed -i -e '$afunc StartDaemonForFuzzing(arguments []string) {\n\tapp := App()\n\t_ = app.Run(arguments)\n}' $SRC/containerd/cmd/containerd/command/main.go
+# Build fuzzer:
+compile_go_fuzzer github.com/containerd/containerd/contrib/fuzz FuzzContainerdImport fuzz_containerd_import github.com/containerd/containerd*
+# Reinstante backup of cmd/containerd:
+mv $SRC/cmd-containerd-backup $SRC/containerd/cmd/containerd
+
+# Compile more fuzzers
+compile_go_fuzzer github.com/containerd/containerd/remotes/docker FuzzFetcher fuzz_fetcher github.com/containerd/containerd*
+compile_go_fuzzer github.com/containerd/containerd/remotes/docker FuzzParseDockerRef fuzz_parse_docker_ref github.com/containerd/containerd*
+compile_go_fuzzer github.com/containerd/containerd/contrib/fuzz FuzzFiltersParse fuzz_filters_parse github.com/containerd/containerd*
+compile_go_fuzzer github.com/containerd/containerd/contrib/fuzz FuzzPlatformsParse fuzz_platforms_parse github.com/containerd/containerd*
+compile_go_fuzzer github.com/containerd/containerd/contrib/fuzz FuzzApply fuzz_apply github.com/containerd/containerd*
+compile_go_fuzzer github.com/containerd/containerd/contrib/fuzz FuzzImportIndex fuzz_import_index github.com/containerd/containerd*
+compile_go_fuzzer github.com/containerd/containerd/contrib/fuzz FuzzCSWalk fuzz_cs_walk github.com/containerd/containerd*
+compile_go_fuzzer github.com/containerd/containerd/contrib/fuzz FuzzArchiveExport fuzz_archive_export github.com/containerd/containerd*
+compile_go_fuzzer github.com/containerd/containerd/contrib/fuzz FuzzParseAuth fuzz_parse_auth github.com/containerd/containerd*
+compile_go_fuzzer github.com/containerd/containerd/contrib/fuzz FuzzParseProcPIDStatus fuzz_parse_proc_pid_status github.com/containerd/containerd*
+compile_go_fuzzer github.com/containerd/containerd/contrib/fuzz FuzzImageStore fuzz_image_store github.com/containerd/containerd*
+compile_go_fuzzer github.com/containerd/containerd/contrib/fuzz FuzzLeaseManager fuzz_lease_manager github.com/containerd/containerd*
+compile_go_fuzzer github.com/containerd/containerd/contrib/fuzz FuzzContainerStore fuzz_container_store github.com/containerd/containerd*
+compile_go_fuzzer github.com/containerd/containerd/contrib/fuzz FuzzContentStore fuzz_content_store github.com/containerd/containerd*
+
+
+# The below fuzzers require more setup than the fuzzers above.
+# We need the binaries from "make".
+wget -c https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protoc-3.11.4-linux-x86_64.zip
+unzip protoc-3.11.4-linux-x86_64.zip -d /usr/local
+
+export CGO_ENABLED=1
+export GOARCH=amd64
+
+# Build runc
+cd $SRC/
+git clone https://github.com/opencontainers/runc --branch release-1.0
+cd runc
+make
+make install
+
+# Build static containerd
+cd $SRC/containerd
+make EXTRA_FLAGS="-buildmode pie" \
+	EXTRA_LDFLAGS='-linkmode external -extldflags "-fno-PIC -static"' \
+	BUILDTAGS="netgo osusergo static_build"
+
+
+mkdir $OUT/containerd-binaries || true
+cd $SRC/containerd/bin && cp * $OUT/containerd-binaries/ && cd -
+
+# Change defaultState and defaultAddress fron /run/containerd-test to /tmp/containerd-test:
+sed -i 's/\/run\/containerd-test/\/tmp\/containerd-test/g' $SRC/containerd/integration/client/client_unix_test.go
+
+cd integration/client
+
+# Rename all *_test.go to *_test_fuzz.go to use their declarations:
+for i in $( ls *_test.go ); do mv $i ./${i%.*}_fuzz.go; done
+
+# Remove windows test to avoid double declarations:
+rm ./client_windows_test_fuzz.go
+rm ./helpers_windows_test_fuzz.go
+compile_go_fuzzer github.com/containerd/containerd/integration/client FuzzCreateContainerNoTearDown fuzz_create_container_no_teardown github.com/containerd/containerd*
+compile_go_fuzzer github.com/containerd/containerd/integration/client FuzzCreateContainerWithTearDown fuzz_create_container_with_teardown github.com/containerd/containerd*
+compile_go_fuzzer github.com/containerd/containerd/integration/client FuzzNoTearDownWithDownload fuzz_no_teardown_with_download github.com/containerd/containerd*
