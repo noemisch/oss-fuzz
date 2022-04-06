766dd92c606690648cf54c1e7e4d79e1b13f012e
psy <patrice.salathe@code-intelligence.com>
2022-04-06 16:31:58 +0200

    jackson-annotations initial integration

---
+++ projects/jackson-annotations/AnnotationsFuzzer.java
@@ -0,0 +1,13 @@
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+
+import com.fasterxml.jackson.annotation.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class AnnotationsFuzzer {
+    public static void fuzzerTestOneInput(FuzzedDataProvider data) {
+        return;
+    }
+}
